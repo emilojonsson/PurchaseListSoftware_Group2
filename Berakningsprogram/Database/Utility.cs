0e1fa8c8e41c68f9a796d24c2c7f1ccf8bd59f36
emil <emil.o.jonsson@gmail.com>
2023-03-22 14:16:17 +0100

    Utility again

--- Berakningsprogram/Database/Utility.cs
+++ Berakningsprogram/Database/Utility.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Npgsql;
+using System.Data;
 
-namespace Berakningsprogram.Database
+namespace TestDatabasePostgres
 {
-    internal class Utility
+    public static class Utility
     {
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=XXXXX;Database=TestDB");
+        }
+        public static void TestConnection()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connected");
+                }
+            }
+        }
+        public static void InsertCreatureToDatabaseViaProcedure(object theObject)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                string query = $@"call insert_data({theObject.ToString()})";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 1 || n == -1)
+                {
+                    Console.WriteLine($"{theObject.GetType()} inserted to database");
+                }
+            }
+        }
+        public static object[] ReadRecord(string typeOfObject, string theCreature)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                var query = $"Select * from {typeOfObject} WHERE Name = '{theCreature}'";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                var dataReader = cmd.ExecuteReader();
+                dataReader.Read();
+                int columns = dataReader.FieldCount;
+                object[] values = new object[columns];
+                dataReader.GetValues(values);
+                return values;
+            }
+        }
     }
 }
